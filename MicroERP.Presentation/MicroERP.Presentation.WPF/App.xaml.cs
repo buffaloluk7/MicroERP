998fe8d2b2bd9da2c50a58f0f4e22b86a8a45b8f
Lukas <buffaloluk7@gmail.com>
2014-05-05 22:41:02 +0200

    adjusted messagebox for unhandled exceptions

--- MicroERP.Presentation/MicroERP.Presentation.WPF/App.xaml.cs
+++ MicroERP.Presentation/MicroERP.Presentation.WPF/App.xaml.cs
@@ -30,19 +30,12 @@ namespace MicroERP.Presentation
         private void Dispatcher_UnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
             e.Handled = true;
-            string errorMessage = string.Format("An application error occurred.\n" +
-                                                 "Please check whether your data is correct and repeat the action." +
-                                                 "If this error occurs again there seems to be a more serious malfunction" +
-                                                 "in the application, and you better close it.\n\nError :{0}\n\nDo you want to continue?\n" +
-                                                 "(if you click Yes you will continue with your work, if you click No the application will close)",
-                                                 e.Exception.Message + (e.Exception.InnerException != null ? "\n" +
-                                                 e.Exception.InnerException.Message : null));
+            string errorMessage = string.Format("An application error occurred.\n\nError message: {0}\n{1}\nDo you want to continue?",
+                                                 e.Exception.Message,
+                                                 (e.Exception.InnerException != null) ? e.Exception.InnerException.Message : null);
 
             if (MessageBox.Show(errorMessage, "Application Error", MessageBoxButton.YesNoCancel, MessageBoxImage.Error) == MessageBoxResult.No)
             {
-                if (MessageBox.Show("WARNING: The application will close. Any changes will not be saved!\nDo you really want to close it?", "Close the application!",
-                                    MessageBoxButton.YesNoCancel, MessageBoxImage.Warning) == MessageBoxResult.Yes)
-                {
                     Application.Current.Shutdown();
                 }
             }
