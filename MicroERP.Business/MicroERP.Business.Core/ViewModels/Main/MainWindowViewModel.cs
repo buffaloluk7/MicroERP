40923b9e15d8cb7d7d6d5975d77c9c36b72a408b
Lukas <buffaloluk7@gmail.com>
2014-05-31 22:07:03 +0200

    Added excluded files

---
+++ MicroERP.Business/MicroERP.Business.Core/ViewModels/Main/MainWindowViewModel.cs
@@ -0,0 +1,82 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Luvi.Service.Browsing;
+using Luvi.Service.Navigation;
+using Luvi.Service.Notification;
+using MicroERP.Business.Core.Services.Interfaces;
+using MicroERP.Business.Core.ViewModels.Main.Commands;
+using MicroERP.Business.Core.ViewModels.Main.Search;
+
+namespace MicroERP.Business.Core.ViewModels.Main
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        #region Fields
+
+        private readonly IBrowsingService browsingService;
+
+        private readonly SearchCustomersViewModel searchCustomersViewModel;
+        private readonly SearchInvoicesViewModel searchInvoicesViewModel;
+
+        private readonly CustomerCommandsViewModel customerCommandsViewModel;
+        private readonly InvoiceCommandsViewModel invoiceCommandsViewModel;
+
+        #endregion
+
+        #region Properties
+
+        public SearchCustomersViewModel SearchCustomersViewModel
+        {
+            get { return this.searchCustomersViewModel; }
+        }
+
+        public SearchInvoicesViewModel SearchInvoicesViewModel
+        {
+            get { return this.searchInvoicesViewModel; }
+        }
+
+        public CustomerCommandsViewModel CustomerCommandsViewModel
+        {
+            get { return this.customerCommandsViewModel; }
+        }
+
+        public InvoiceCommandsViewModel InvoiceCommandsViewModel
+        {
+            get { return this.invoiceCommandsViewModel; }
+        }
+
+        public RelayCommand RepositoryCommand
+        {
+            get;
+            private set;
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public MainWindowViewModel(ICustomerService customerService, IInvoiceService invoiceService, INotificationService notificationService, INavigationService navigationService, IBrowsingService browsingService, SearchCustomersViewModel searchCustomersViewModel, SearchInvoicesViewModel searchInvoicesViewModel)
+        {
+            this.browsingService = browsingService;
+
+            this.searchCustomersViewModel = searchCustomersViewModel;
+            this.searchInvoicesViewModel = searchInvoicesViewModel;
+
+            this.customerCommandsViewModel = new CustomerCommandsViewModel(customerService, notificationService, navigationService, searchCustomersViewModel);
+            this.invoiceCommandsViewModel = new InvoiceCommandsViewModel(invoiceService, searchInvoicesViewModel);
+
+            this.RepositoryCommand = new RelayCommand(onRepositoryExecuted);
+        }
+
+        #endregion
+
+        #region Command Implementations
+
+        private async void onRepositoryExecuted()
+        {
+            await this.browsingService.OpenLinkAsync("https://github.com/buffaloluk7/micro_erp.git");
+        }
+
+        #endregion
+    }
+}
---
+++ MicroERP.Business/MicroERP.Business.Core/ViewModels/Main/Search/SearchInvoicesViewModel.cs
@@ -0,0 +1,151 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using MicroERP.Business.Core.Services.Interfaces;
+using MicroERP.Business.Core.ViewModels.Models;
+using MicroERP.Business.Core.ViewModels.SearchBox;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MicroERP.Business.Core.ViewModels.Main.Search
+{
+    public class SearchInvoicesViewModel : ObservableObject
+    {
+        #region Fields
+
+        private readonly IInvoiceService invoiceService;
+        private IEnumerable<InvoiceModelViewModel> invoices;
+        private InvoiceModelViewModel selectedInvoice;
+
+        private DateTime? minDate;
+        private DateTime? maxDate;
+        private double? minPrice;
+        private double? maxPrice;
+
+        #endregion
+
+        #region Properties
+
+        public DateTime? MinDate
+        {
+            get { return this.minDate; }
+            set
+            {
+                base.Set<DateTime?>(ref this.minDate, value);
+                this.SearchInvoicesCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public DateTime? MaxDate
+        {
+            get { return this.maxDate; }
+            set
+            {
+                base.Set<DateTime?>(ref this.maxDate, value);
+                this.SearchInvoicesCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public double? MinPrice
+        {
+            get { return this.minPrice; }
+            set
+            {
+                base.Set<double?>(ref this.minPrice, value);
+                this.SearchInvoicesCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public double? MaxPrice
+        {
+            get { return this.maxPrice; }
+            set
+            {
+                base.Set<double?>(ref this.maxPrice, value);
+                this.SearchInvoicesCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public CustomerSearchBoxViewModel CustomerSearchBoxViewModel
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerable<InvoiceModelViewModel> Invoices
+        {
+            get { return this.invoices; }
+            set { base.Set<IEnumerable<InvoiceModelViewModel>>(ref this.invoices, value); }
+        }
+
+        public InvoiceModelViewModel SelectedInvoice
+        {
+            get { return this.selectedInvoice; }
+            set { base.Set<InvoiceModelViewModel>(ref this.selectedInvoice, value); }
+        }
+
+        #endregion
+
+        #region Commands
+
+        public RelayCommand SearchInvoicesCommand
+        {
+            get;
+            private set;
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public SearchInvoicesViewModel(IUnityContainer container, IInvoiceService invoiceService)
+        {
+            this.invoiceService = invoiceService;
+            this.SearchInvoicesCommand = new RelayCommand(this.onSearchInvoicesExecuted, this.onSearchInvoicesCanExecute);
+
+            this.CustomerSearchBoxViewModel = container.Resolve<CustomerSearchBoxViewModel>();
+            this.CustomerSearchBoxViewModel.PropertyChanged += ((s, e) => 
+            {
+                if (e.PropertyName == "SelectedCustomer")
+                {
+                    this.SearchInvoicesCommand.RaiseCanExecuteChanged();
+                }
+            });
+
+            #if DEBUG
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+                this.CustomerSearchBoxViewModel.SearchQuery = "lukas";
+                this.onSearchInvoicesExecuted();
+            }
+            #endif
+        }
+
+        #endregion
+
+        #region Command Implementations
+
+        private bool onSearchInvoicesCanExecute()
+        {
+            if (this.CustomerSearchBoxViewModel.SelectedCustomer == null)
+            {
+                this.Invoices = null;
+                return false;
+            }
+
+            return true;
+        }
+
+        private async void onSearchInvoicesExecuted()
+        {
+            var customer = this.CustomerSearchBoxViewModel.SelectedCustomer == null ? null : this.CustomerSearchBoxViewModel.SelectedCustomer.Model;
+            int? customerID = customer == null ? default(int?) : customer.ID;
+
+            var invoices = await this.invoiceService.Search(customerID, this.minDate, this.maxDate, this.minPrice, this.maxPrice);
+            this.Invoices = invoices.Select(invoice => new InvoiceModelViewModel(invoice));
+        }
+
+        #endregion
+    }
+}
