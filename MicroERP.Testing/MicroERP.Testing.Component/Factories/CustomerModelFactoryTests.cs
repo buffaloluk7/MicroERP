6971367c6da57ade59aa6325b18c3d1b1de856c9
Lukas Streiter <buffaloluk7@gmail.com>
2014-06-07 13:47:19 +0200

    Added factory unit tests

---
+++ MicroERP.Testing/MicroERP.Testing.Component/Factories/CustomerModelFactoryTests.cs
@@ -0,0 +1,35 @@
+﻿using MicroERP.Business.Core.Factories;
+using MicroERP.Business.Domain.Enums;
+using MicroERP.Business.Domain.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MicroERP.Testing.Component.Factories
+{
+    [TestClass]
+    public class CustomerModelFactoryTests
+    {
+        [TestMethod]
+        public void Test_CompanyType()
+        {
+            var model = CustomerModelFactory.FromType(CustomerType.Company);
+
+            Assert.IsInstanceOfType(model, typeof(CompanyModel));
+        }
+
+        [TestMethod]
+        public void Test_PersonType()
+        {
+            var model = CustomerModelFactory.FromType(CustomerType.Person);
+
+            Assert.IsInstanceOfType(model, typeof(PersonModel));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Test_InvalidType()
+        {
+            CustomerModelFactory.FromType(CustomerType.None);
+        }
+    }
+}
---
+++ MicroERP.Testing/MicroERP.Testing.Component/Factories/RepositoryFactoryTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MicroERP.Business.Core.Factories;
+
+namespace MicroERP.Testing.Component.Factories
+{
+    [TestClass]
+    public class RepositoryFactoryTests
+    {
+        [TestMethod]
+        public void Test_CreateRepositories()
+        {
+            var repository = RepositoryFactory.CreateRepositories();
+
+            Assert.IsNotNull(repository.Item1);
+            Assert.IsNotNull(repository.Item2);
+        }
+    }
+}
